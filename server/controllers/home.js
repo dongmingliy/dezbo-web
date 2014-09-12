a286d13078bbd740705b6272ca540c641a83817d
dongmingliy <louis.dm.li@gmail.com>
2014-09-11 21:03:18 -0700

    social media changes

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -5,7 +5,7 @@
  */
 
 module.exports.controller = function (app) {
-  app.get('/comingsoon', function (req, res) {
+  app.get('/', function (req, res) {
     if (req.user) {
       return res.redirect('home/index');
     }
