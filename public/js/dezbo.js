818c7ea66e84fd324718cc744a58a00b0c199022
louis <louis.dm.li@gmail.com>
2014-09-24 01:25:33 -0400

    ga

--- public/js/dezbo.js
+++ public/js/dezbo.js
@@ -120,11 +120,11 @@ dezboapp.controller('comingsoonCtrl', ['$scope','$http','$timeout',
 ]);
 
 'use strict';
-dezboapp.controller('gameCtrl', ['$scope','$http',
-  function($scope, $http) {
+dezboapp.controller('gameCtrl', ['$scope','$http','$window',
+  function($scope, $http,$window) {
     $scope.celebItems =[
-      {"itemTitle":"Red Couch on the Ellen Show", "itemURL":"img/shop/ellen1.png","celebName":"Ellen Degeneres", "celebURL": "img/game/ellen_head.png"},
-      {"itemTitle":"NFL's Game Ball", "itemURL":"img/coming_soon/NFL.png","celebName":"Pharrell Williams", "celebURL": "img/logo_o.png"}
+      {"id": 0, "itemTitle":"Red Couch on the Ellen Show", "itemURL":"img/shop/ellen1.png","celebName":"Ellen Degeneres", "celebURL": "img/game/ellen_head.png"},
+      {"id": 1, "itemTitle":"NFL's Game Ball", "itemURL":"img/coming_soon/NFL.png","celebName":"Pharrell Williams", "celebURL": "img/logo_o.png"}
     ];
 //    $http.get('celebItems.json').
 //      success(function(data){
@@ -137,13 +137,17 @@ dezboapp.controller('gameCtrl', ['$scope','$http',
     $scope.itemURL ='img/game/ellen_head.png';
     $scope.counter = 0;
     $scope.celebItem = $scope.celebItems[$scope.counter];
-    $scope.changeItem = function() {
+    $scope.changeItem = function(voteValue) {
       if($scope.counter == 1){
         $scope.counter = 0;
       } else {
         $scope.counter = 1;
       }
-    $scope.celebItem = $scope.celebItems[$scope.counter];
+      // send google analytics the current item's vote
+      if($window.ga){
+        ga('send', 'event', 'voteitem', $scope.celebItem.id, $scope.celebItem.itemTitle , voteValue);
+      }
+      $scope.celebItem = $scope.celebItems[$scope.counter];
     };
   }
 ]);
