a6dfe76127a78de4c95600f51fe0b378ddaf3285
louis <louis.dm.li@gmail.com>
2014-09-25 22:34:45 -0400

    game

--- client/controller/gameCtrl.js
+++ client/controller/gameCtrl.js
@@ -1,12 +1,33 @@
 'use strict';
+var randnums = [0,1,2,3,4,5,6];
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+shuffle(randnums);
 dezboapp.controller('gameCtrl', ['$scope', '$http', '$window', '$timeout',
   function ($scope, $http, $window, $timeout) {
+    var counter = 0;
     $scope.celebItems = [];
     $http.get('/celebItems').
       success(function (data) {
         $scope.celebItems = data;
         $scope.counter = 0;
-        $scope.celebItem = $scope.celebItems[$scope.counter];
+        $scope.celebItem = $scope.celebItems[randnums[counter]];
         $scope.showProgress = false;
       })
       .error(function (data) {
@@ -16,32 +37,29 @@ dezboapp.controller('gameCtrl', ['$scope', '$http', '$window', '$timeout',
         });
       });
 
-
     $scope.changeItem = function (voteValue) {
+      counter++;
       $scope.inProgress = true;
-      if ($scope.counter == 1) {
-        $scope.counter = 0;
-      } else {
-        $scope.counter = 1;
-      }
-      // send google analytics the current item's vote
-      if ($window.ga) {
-        ga('send', 'event', 'voteitem', $scope.celebItem.id, $scope.celebItem.itemTitle, voteValue);
-      }
-      $scope.showProgress = true;
-      $scope.votePercentage = Math.floor((Math.random() * 100) + 0);
-      var nextImage = function () {
-        $scope.celebItem = $scope.celebItems[$scope.counter];
-        $scope.showProgress = false;
-        $scope.inProgress = false;
-        $scope.votePercentage = 0;
-      };
+      if ($scope.celebItems[randnums[counter]]) {
+        // send google analytics the current item's vote
+        if ($window.ga) {
+          ga('send', 'event', 'voteitem', $scope.celebItem.id, $scope.celebItem.itemTitle, voteValue);
+        }
+        $scope.showProgress = true;
+        $scope.votePercentage = Math.floor((Math.random() * 100) + 0);
+        var nextImage = function () {
+          $scope.celebItem = $scope.celebItems[randnums[counter]];
+          $scope.showProgress = false;
+          $scope.inProgress = false;
+          $scope.votePercentage = 0;
+        };
 
-      $timeout(nextImage, 1500);
-      $http({method: 'Post', url: '/voteitem', data: {greeting: 'hi'}}).
-        success(function(data, status, headers, config) {
-          alert(data);
-        });
+        $timeout(nextImage, 1500);
+//        $http({method: 'Post', url: '/voteitem', data: {greeting: 'hi'}}).
+//          success(function (data, status, headers, config) {
+//            alert(data);
+//          });
+      }
     };
   }
 ]);
--- server/controllers/game.js
+++ server/controllers/game.js
@@ -8,7 +8,20 @@ module.exports.controller = function (app) {
 
   var celebItems = [
     {'id': '0', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/shop/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-    {'id': '1', 'itemTitle': 'Snoop Dog\'s Stage Mic', 'itemURL': 'img/game/snoopmic.png', 'celebName': 'Snoop Dog', 'celebURL': 'img/game/snoop_face.png'}
+    {'id': '1', 'itemTitle': 'Snoop Dogg\'s Mic', 'itemURL': 'img/game/snoopmic.png', 'celebName': 'Snoop Dog', 'celebURL': 'img/game/snoop_face.png'},
+    {'id': '2', 'itemTitle': 'Signed Beats by Dre', 'itemURL': 'img/game/beatsdre.png', 'celebName': 'Dr.Dre', 'celebURL': 'img/game/dre.png'},
+    {'id': '3', 'itemTitle': 'Ariana Grande\'s Signed White Mic', 'itemURL': 'img/game/arianawhitemic.png', 'celebName': 'Ariana Grande', 'celebURL': 'img/game/arianaface.png'},
+    {'id': '4', 'itemTitle': 'Cristiano Ronaldo\'s Signed Nike Mercurial Superfly', 'itemURL': 'img/game/ronaldoshoe.png', 'celebName': 'Cristiano Ronaldo', 'celebURL': 'img/game/ronaldoface.png'},
+    {'id': '7', 'itemTitle': 'Sheldon\'s laptop', 'itemURL': 'img/game/TBBTSheldonlaptop.png', 'celebName': 'Jim Parsons', 'celebURL': 'img/game/TBBT.png'},
+    {'id': '8', 'itemTitle': 'Frank Underwood\'s typewriter', 'itemURL': 'img/game/HOCtypewriter.png', 'celebName': 'Kevin Spacey', 'celebURL': 'img/game/HOC.png'}
+//    {'id': '7', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '8', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '9', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '10', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '11', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '12', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '13', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+//    {'id': '14', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
   ];
   app.get('/celebItems', function (req, res) {
     res.json(celebItems);
