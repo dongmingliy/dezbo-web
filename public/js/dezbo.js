c8ee5304e23dbe7930f34a805fcc588517ab54f1
louis <louis.dm.li@gmail.com>
2014-09-26 01:27:55 -0400

    game

--- public/js/dezbo.js
+++ public/js/dezbo.js
@@ -120,7 +120,7 @@ dezboapp.controller('comingsoonCtrl', ['$scope','$http','$timeout',
 ]);
 
 'use strict';
-var randnums = [0,1,2,3,4,5,6];
+var randomItems = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
 
@@ -139,19 +139,21 @@ function shuffle(array) {
 
   return array;
 }
-shuffle(randnums);
+shuffle(randomItems);
+
 dezboapp.controller('gameCtrl', ['$scope', '$http', '$window', '$timeout',
   function ($scope, $http, $window, $timeout) {
-    var counter = 0;
+    $scope.counter = 0;
     $scope.celebItems = [];
     $http.get('/celebItems').
       success(function (data) {
         $scope.celebItems = data;
         $scope.counter = 0;
-        $scope.celebItem = $scope.celebItems[randnums[counter]];
+        $scope.celebItem = $scope.celebItems[randomItems[$scope.counter]];
         $scope.showProgress = false;
       })
-      .error(function (data) {
+      .error(function (data)
+      {
         console.log(data);
         $scope.$apply(function () {
           $location.path("/comingsoon");
@@ -159,9 +161,9 @@ dezboapp.controller('gameCtrl', ['$scope', '$http', '$window', '$timeout',
       });
 
     $scope.changeItem = function (voteValue) {
-      counter++;
+      $scope.counter++;
       $scope.inProgress = true;
-      if ($scope.celebItems[randnums[counter]]) {
+      if ($scope.celebItems[randomItems[$scope.counter]]) {
         // send google analytics the current item's vote
         if ($window.ga) {
           ga('send', 'event', 'voteitem', $scope.celebItem.id, $scope.celebItem.itemTitle, voteValue);
@@ -169,17 +171,19 @@ dezboapp.controller('gameCtrl', ['$scope', '$http', '$window', '$timeout',
         $scope.showProgress = true;
         $scope.votePercentage = Math.floor((Math.random() * 100) + 0);
         var nextImage = function () {
-          $scope.celebItem = $scope.celebItems[randnums[counter]];
+          $scope.celebItem = $scope.celebItems[randomItems[$scope.counter]];
           $scope.showProgress = false;
           $scope.inProgress = false;
           $scope.votePercentage = 0;
         };
 
-        $timeout(nextImage, 1500);
+        $timeout(nextImage, 200);
 //        $http({method: 'Post', url: '/voteitem', data: {greeting: 'hi'}}).
 //          success(function (data, status, headers, config) {
 //            alert(data);
 //          });
+      } else {
+        $scope.inProgress = false;
       }
     };
   }
--- server/controllers/game.js
+++ server/controllers/game.js
@@ -8,20 +8,19 @@ module.exports.controller = function (app) {
 
   var celebItems = [
     {'id': '0', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/shop/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-    {'id': '1', 'itemTitle': 'Snoop Dogg\'s Mic', 'itemURL': 'img/game/snoopmic.png', 'celebName': 'Snoop Dog', 'celebURL': 'img/game/snoop_face.png'},
+    {'id': '1', 'itemTitle': 'Snoop Dogg\'s Mic', 'itemURL': 'img/game/snoopmic.png', 'celebName': 'Snoop Dogg', 'celebURL': 'img/game/snoop_face.png'},
     {'id': '2', 'itemTitle': 'Signed Beats by Dre', 'itemURL': 'img/game/beatsdre.png', 'celebName': 'Dr.Dre', 'celebURL': 'img/game/dre.png'},
     {'id': '3', 'itemTitle': 'Ariana Grande\'s Signed White Mic', 'itemURL': 'img/game/arianawhitemic.png', 'celebName': 'Ariana Grande', 'celebURL': 'img/game/arianaface.png'},
     {'id': '4', 'itemTitle': 'Cristiano Ronaldo\'s Signed Nike Mercurial Superfly', 'itemURL': 'img/game/ronaldoshoe.png', 'celebName': 'Cristiano Ronaldo', 'celebURL': 'img/game/ronaldoface.png'},
-    {'id': '7', 'itemTitle': 'Sheldon\'s laptop', 'itemURL': 'img/game/TBBTSheldonlaptop.png', 'celebName': 'Jim Parsons', 'celebURL': 'img/game/TBBT.png'},
-    {'id': '8', 'itemTitle': 'Frank Underwood\'s typewriter', 'itemURL': 'img/game/HOCtypewriter.png', 'celebName': 'Kevin Spacey', 'celebURL': 'img/game/HOC.png'}
-//    {'id': '7', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '8', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '9', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '10', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '11', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '12', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '13', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
-//    {'id': '14', 'itemTitle': 'Red Couch on the Ellen Show', 'itemURL': 'img/game/ellen1.png', 'celebName': 'Ellen Degeneres', 'celebURL': 'img/game/ellen_head.png'},
+    {'id': '5', 'itemTitle': 'Leo Messi\'s Signed FCB Jersey', 'itemURL': 'img/game/messijersey.png', 'celebName': 'Leo Messi', 'celebURL': 'img/game/messiface.png'},
+    {'id': '6', 'itemTitle': 'adidas Brazuca 2014 FIFA World Cup Official Match Ball ', 'itemURL': 'img/game/FIFAball.png', 'celebName': 'FIFAball', 'celebURL': 'img/game/FIFA.png'},
+    {'id': '7', 'itemTitle': 'Sheldon\'s laptop', 'itemURL': 'img/game/TBBTSheldonlaptop.png', 'celebName': 'The Big Bang Theory', 'celebURL': 'img/game/TBBT.png'},
+    {'id': '8', 'itemTitle': 'Frank Underwood\'s typewriter', 'itemURL': 'img/game/HOCtypewriter.png', 'celebName': 'House of Cards', 'celebURL': 'img/game/HOC.png'},
+    {'id': '9', 'itemTitle': 'Jax Teller\'s President Jacket', 'itemURL': 'img/game/SOAjacket.png', 'celebName': 'Sons of Anarchy', 'celebURL': 'img/game/SOA.png'},
+    {'id': '10', 'itemTitle': 'NFL\'s 2014 Season Premiere Game Ball', 'itemURL': 'img/game/NFL_ball.png', 'celebName': 'NFL', 'celebURL': 'img/game/NFL.png'},
+    {'id': '11', 'itemTitle': 'Lebron James Signed Basketball', 'itemURL': 'img/game/Lebronball.png', 'celebName': 'Lebron James', 'celebURL': 'img/game/lebronface.png'},
+    {'id': '12', 'itemTitle': 'Sidney Crosby Signed Hockey Stick', 'itemURL': 'img/game/Sidneystick.png', 'celebName': 'Sidney Crosby', 'celebURL': 'img/game/sidneyface.png'},
+    {'id': '13', 'itemTitle': 'Tony Hawk Signed Skateboard', 'itemURL': 'img/game/Tonyboard.png', 'celebName': 'Tony Hawk', 'celebURL': 'img/game/tony.png'}
   ];
   app.get('/celebItems', function (req, res) {
     res.json(celebItems);
