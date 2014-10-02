0453bf7f166910002156be46a0b8ae1157324fb2
louis <louis.dm.li@gmail.com>
2014-10-02 00:13:44 -0400

    game
    game

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -5,6 +5,9 @@
  */
 var ItemVote = require('../models/itemvotes');
 var UserEmail = require('../models/useremail');
+var config = require('../config/config');
+var nodemailer = require('nodemailer');
+
 module.exports.controller = function (app) {
 
   var celebItems = [
@@ -100,16 +103,72 @@ module.exports.controller = function (app) {
 
   app.post('/gamesignup', function (req, res, next) {
 
-    var userEmail = new UserEmail({
-      email: req.body.email.toLowerCase()
+    var emailAddress = req.body.email;
+    // Create a workflow
+    var workflow = new (require('events').EventEmitter)();
+
+    /**
+     * Step 1: Save User Email to database
+     * */
+
+    workflow.on('saveUserEmail', function () {
+      var userEmail = new UserEmail({
+        email: req.body.email.toLowerCase()
+      });
+      // save user email
+      userEmail.save(function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      // next step
+      workflow.emit('sendComingsoonEmail');
     });
-    // save user email
-    userEmail.save(function (err) {
-      if (err) {
-        console.log(err);
-      }
+
+    /**
+     * Step 2: Send a coming soon email
+     * */
+    workflow.on('sendComingsoonEmail', function () {
+
+      // Create reusable transporter object using SMTP transport
+      var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: config.gmail.user,
+          pass: config.gmail.password
+        }
+      });
+
+      // Render HTML to send using .jade mail template (just like rendering a page)
+      res.render('mail/comingsoon', {
+        name: 'Dear fan'
+      }, function (err, html) {
+        if (err) {
+          req.flash('error', { msg: err });
+        }
+        else {
+          // Create email
+          var mailOptions = {
+            to: emailAddress,
+            from: config.smtp.name + ' <' + config.smtp.address + '>',
+            subject: 'Welcome to Dezbo!',
+            html: html
+          };
+
+          // Send email
+          transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+              req.flash('error', { msg: err });
+            } else {
+              req.flash('info', { msg: 'Thanks for signing up! You rock!' });
+              res.redirect('/result');
+            }
+          });
+        }
+      });
     });
-    req.flash('info', { msg: 'Thanks for signing up! You rock!' });
-    res.redirect('/result');
+
+    workflow.emit('saveUserEmail');
+
   });
 };
