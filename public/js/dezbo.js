47aa90ec52a6d2dd2e767f053526557e66b85ab2
louis <louis.dm.li@gmail.com>
2014-10-30 09:39:46 -0400

    ellen page css

--- public/js/dezbo.js
+++ public/js/dezbo.js
@@ -239,27 +239,60 @@ dezboapp.controller('homeCtrl', ['$scope', '$modal',
       charityName: "The Gentle Barn",
       ending: "Nov 25, 2014"
     },
-      {
-        imageURL: "img/shop/pharrell1_small.png",
-        celebName: "Pharrell's",
-        itemName: "Tall Hat",
-        charityName: "From One Hand To Another",
-        ending: "Dec 11, 2014"
-      },
-      {
-        imageURL: "img/shop/gaga1_small.png",
-        celebName: "Lady Gaga’s ",
-        itemName: "Red Carpet Heels",
-        charityName: "Born This Way Foundation",
-        ending: "Dec 15, 2014"
-      },
-      {
-        imageURL: "img/shop/snoop1_small.png",
-        celebName: "Snoop’s ",
-        itemName: "Stage Mic",
-        charityName: "Youth Football League Foundation",
-        ending: "Nov 25, 2014"
-      }];
+    {
+      imageURL: "img/shop/pharrell1_small.png",
+      celebName: "Pharrell's",
+      itemName: "Tall Hat",
+      charityName: "From One Hand To Another",
+      ending: "Dec 11, 2014"
+    },
+    {
+      imageURL: "img/shop/gaga1_small.png",
+      celebName: "Lady Gaga’s ",
+      itemName: "Red Carpet Heels",
+      charityName: "Born This Way Foundation",
+      ending: "Dec 15, 2014"
+    },
+    {
+      imageURL: "img/shop/snoop1_small.png",
+      celebName: "Snoop’s ",
+      itemName: "Stage Mic",
+      charityName: "Youth Football League Foundation",
+      ending: "Nov 25, 2014"
+    }];
+
+    $scope.biddingitems = [{
+      imageURL: 'img/shop/ellen2_small.png',
+      celebName: "Ellen's",
+      itemName: "signed Oscars Selfie",
+      currentBid: "$350",
+      charityName: "The Gentle Barn",
+      startTime: "1401000000101"
+    },
+    {
+      imageURL: "img/shop/snoop5_small.png",
+      celebName: "Snoop’s",
+      itemName: "Sunglasses",
+      currentBid: "$250",
+      charityName: "Youth Football League Foundation",
+      startTime: "1401000000101"
+    },
+    {
+      imageURL: "img/shop/gaga2_small.png",
+      celebName: "Lady Gaga’s",
+      itemName: "Gloves",
+      currentBid: "$784",
+      charityName: "Born This Way Foundation",
+      startTime: "1401000000101"
+    },
+    {
+      imageURL: "img/shop/kristine1_small.png",
+      celebName: "Kristen Bell’s",
+      itemName: "Camera",
+      currentBid: "$1500",
+      charityName: "PETA",
+      startTime: "1401000000101"
+    }];
 //    $scope.showYoutubeModal = function ($scope,$http, $modal) {
 //      $modal.open({
 //        templateUrl: '/home/youtube',
@@ -341,10 +374,22 @@ dezboapp.controller('shopController', ['$scope',
 
 'use strict';
 
-dezboapp.directive('itembox', function() {
+dezboapp.directive('biddingbox', function() {
   return {
     restrict: 'E',
-    templateUrl: '/directive/itembox',
+    templateUrl: '/directive/biddingbox',
+    scope: {
+      i: "="
+    }
+  };
+});
+
+'use strict';
+
+dezboapp.directive('rafflebox', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/directive/rafflebox',
     scope: {
       i: "="
     }
