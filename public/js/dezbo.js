7c269eb7efa660faaa0ff37f0f6a8c61b0f47a69
louis <louis.dm.li@gmail.com>
2014-11-11 10:18:36 -0500

    banner and slider

--- public/js/dezbo.js
+++ public/js/dezbo.js
@@ -2515,7 +2515,7 @@ angular.module("ui.bootstrap",["ui.bootstrap.tpls","ui.bootstrap.transition","ui
 
 jQuery.fn.liScroll = function(settings) {
   settings = jQuery.extend({
-    travelocity: 0.07
+    travelocity: 0.05
   }, settings);
   return this.each(function(){
     var $strip = jQuery(this);
@@ -2526,9 +2526,9 @@ jQuery.fn.liScroll = function(settings) {
     });
     var $mask = $strip.wrap("<div class='mask'></div>");
     var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");
-    var containerWidth = $strip.parent().parent().width();	//a.k.a. 'mask' width
+    var containerWidth = $strip.width();	//a.k.a. 'mask' width
     $strip.width(stripWidth);
-    var totalTravel = stripWidth+containerWidth;
+    var totalTravel = containerWidth;
     var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye
     function scrollnews(spazio, tempo){
       $strip.animate({left: '-='+ spazio}, tempo, "linear", function(){$strip.css("left", containerWidth); scrollnews(totalTravel, defTiming);});
